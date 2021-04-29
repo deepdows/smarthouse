e988b2ac51c0abaa063553e0a9155578bc3be22f
deepdows <deepdows@gmail.com>
2021-04-29 22:02:23 +0300

    test

--- smarthouse.py
+++ smarthouse.py
@@ -9,27 +9,30 @@ app.jinja_env.lstrip_blocks = True
 
 bootstrap = Bootstrap(app)
 
-settings = None
+# settings = None
 
-@app.route('/data', methods=['GET','POST'])
-def get_data():
-    if request.method == 'POST':
-        global settings
-        data = request.get_json()
-        if data and 'api' in data and data['api'] == '1234321':
-            return jsonify(settings)
-        return ('', 204)
-    if request.method == 'GET':
-        return jsonify(data)
+# @app.route('/data', methods=['GET','POST'])
+# def get_data():
+#     data = None
+#     if request.method == 'POST':
+#         global settings
+#         data = request.get_json()
+#         if data and 'api' in data and data['api'] == '1234321':
+#             print(data)
+#             return jsonify(settings)
+#         return ('', 204)
+#     if request.method == 'GET':
+#         print(data)
+#         return jsonify(data)
 
-@app.route('/settings', methods=['POST'])
-def settings():
-    global settings
+# @app.route('/settings', methods=['POST'])
+# def upload_settings():
+#     global settings
     
-    data = request.args
-    if data:
-        settings = data['brightness']
-        return ('Success')
+#     data = request.args
+#     if data:
+#         settings = data['brightness']
+#         return ('Success')
 
 @app.route('/')
 def index():

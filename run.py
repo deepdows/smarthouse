27a6543df714aa80d2043f849931cd1eea75069d
deepdows <deepdows@gmail.com>
2021-04-29 23:35:07 +0300

    bootstrap

--- run.py
+++ run.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
-bootstrap = Bootstrap(app)
-
 # settings = None
 
 # @app.route('/data', methods=['GET','POST'])

1272ad6235fc086b0296dcaf231f0f32e810ee55
deepdows <deepdows@gmail.com>
2021-05-09 17:40:50 +0300

    fixed minimum

--- smarthouse/analyzer_routes.py
+++ smarthouse/analyzer_routes.py
@@ -118,7 +118,7 @@ def graph(name, day):
                 name_lower=name, data=data, 
                 time=time, title=f'Analyzer - {name.capitalize()}',
                 one_day_ahead=one_day_ahead.strftime('%Y%m%d'), 
-                one_day_ago=one_day_ago.strftime('%Y%m%d'), min_y=min_y+0.5,
+                one_day_ago=one_day_ago.strftime('%Y%m%d'), min_y=min_y-0.5,
                 one_day_ahead_dashed=one_day_ahead.strftime('%Y-%m-%d'), 
                 one_day_ago_dashed=one_day_ago.strftime('%Y-%m-%d'),
                 maps=[['Home', 'index'], ['Analyzer', 'analyzer'], 
